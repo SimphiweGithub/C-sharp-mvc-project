8b372f456fb9574d6936a3e0a5e01ff90cc624c1
SimphiweGithub <Simphiwe0105@gmail.com>
2025-03-08 07:47:03 +0200

    mvc stuff

---
+++ Controllers/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Controllers
+{
+    public class Expense
+    {
+        public int ID { get; set; }
+        public double Value { get; set; }
+        [Required]//we set this string to nullale but the enforce it so that a datatype is required
+        //? allows you to have a null value
+        public string? Description { get; set; }
+    }
+}
